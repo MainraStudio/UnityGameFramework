0ebc9edb07415b6eff8047374684244acdc4d605
farismiqdad01 <farismiqdad01@gmail.com>
2025-01-19 15:05:38 +0700

    update tweener

--- Assets/_Game/Scripts/Utility/Editor/TweenerEditor.cs
+++ Assets/_Game/Scripts/Utility/Editor/TweenerEditor.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 [CustomEditor(typeof(Tweener))]
 public class TweenerEditor : Editor
 {
-    SerializedProperty tweenAnimations;
+    private SerializedProperty tweenAnimations;
     private bool[] foldouts;
 
     private void OnEnable()
     {
         tweenAnimations = serializedObject.FindProperty("tweenAnimations");
-        foldouts = new bool[tweenAnimations.arraySize];
+        UpdateFoldoutsArray();
+    }
+
+    private void UpdateFoldoutsArray()
+    {
+        foldouts = new bool[tweenAnimations?.arraySize ?? 0];
     }
 
     public override void OnInspectorGUI()
@@ -19,56 +24,89 @@ public class TweenerEditor : Editor
 
         Tweener tweener = (Tweener)target;
 
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("debugMode"));
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("runOnStart"));
         EditorGUILayout.PropertyField(serializedObject.FindProperty("useUnscaledTime"));
 
         EditorGUILayout.Space();
         EditorGUILayout.LabelField("Tween Animations", EditorStyles.boldLabel);
 
-        for (int i = 0; i < tweenAnimations.arraySize; i++)
+        if (foldouts.Length != (tweenAnimations?.arraySize ?? 0))
         {
-            SerializedProperty tween = tweenAnimations.GetArrayElementAtIndex(i);
-            SerializedProperty tweenType = tween.FindPropertyRelative("tweenType");
+            UpdateFoldoutsArray();
+        }
 
-            foldouts[i] = EditorGUILayout.Foldout(foldouts[i], $"Animation {i + 1}", true);
-            if (foldouts[i])
+        if (tweenAnimations != null)
+        {
+            for (int i = 0; i < tweenAnimations.arraySize; i++)
             {
-                EditorGUILayout.PropertyField(tweenType);
+                SerializedProperty tween = tweenAnimations.GetArrayElementAtIndex(i);
+                SerializedProperty tweenType = tween.FindPropertyRelative("tweenType");
 
-                switch ((Tweener.TweenSettings.TweenType)tweenType.enumValueIndex)
+                foldouts[i] = EditorGUILayout.Foldout(foldouts[i], $"Animation {i + 1}", true);
+                if (foldouts[i])
                 {
-                    case Tweener.TweenSettings.TweenType.Move:
-                    case Tweener.TweenSettings.TweenType.Scale:
-                    case Tweener.TweenSettings.TweenType.Rotate:
-                        EditorGUILayout.PropertyField(tween.FindPropertyRelative("targetValue"));
-                        break;
-                    case Tweener.TweenSettings.TweenType.Fade:
-                        EditorGUILayout.PropertyField(tween.FindPropertyRelative("targetAlpha"));
-                        break;
-                    case Tweener.TweenSettings.TweenType.Color:
-                        EditorGUILayout.PropertyField(tween.FindPropertyRelative("targetColor"));
-                        EditorGUILayout.PropertyField(tween.FindPropertyRelative("isUIElement"));
-                        break;
+                    EditorGUILayout.PropertyField(tweenType);
+
+                    switch ((Tweener.TweenSettings.TweenType)tweenType.enumValueIndex)
+                    {
+                        case Tweener.TweenSettings.TweenType.Move:
+                        case Tweener.TweenSettings.TweenType.Scale:
+                        case Tweener.TweenSettings.TweenType.Rotate:
+                            EditorGUILayout.PropertyField(tween.FindPropertyRelative("targetValue"));
+                            break;
+                        case Tweener.TweenSettings.TweenType.Fade:
+                            EditorGUILayout.PropertyField(tween.FindPropertyRelative("targetAlpha"));
+                            break;
+                        case Tweener.TweenSettings.TweenType.Color:
+                            EditorGUILayout.PropertyField(tween.FindPropertyRelative("targetColor"));
+                            break;
+                    }
+
+                    EditorGUILayout.PropertyField(tween.FindPropertyRelative("isUIElement"));
+                    EditorGUILayout.PropertyField(tween.FindPropertyRelative("duration"));
+                    EditorGUILayout.PropertyField(tween.FindPropertyRelative("delay"));
+
+                    SerializedProperty useCustomCurve = tween.FindPropertyRelative("useCustomCurve");
+                    EditorGUILayout.PropertyField(useCustomCurve);
+
+                    if (useCustomCurve.boolValue)
+                    {
+                        EditorGUILayout.PropertyField(tween.FindPropertyRelative("customCurve"));
+                    }
+                    else
+                    {
+                        EditorGUILayout.PropertyField(tween.FindPropertyRelative("easeType"));
+                    }
+
+                    SerializedProperty loop = tween.FindPropertyRelative("loop");
+                    EditorGUILayout.PropertyField(loop);
+
+                    if (loop.boolValue)
+                    {
+                        EditorGUILayout.PropertyField(tween.FindPropertyRelative("loopCount"));
+                        EditorGUILayout.PropertyField(tween.FindPropertyRelative("pingpong"));
+                    }
+
+                    EditorGUILayout.PropertyField(tween.FindPropertyRelative("startOnActive"));
+                    EditorGUILayout.PropertyField(tween.FindPropertyRelative("OnTweenComplete"));
+
+                    if (GUILayout.Button("Remove Tween Animation"))
+                    {
+                        tweenAnimations.DeleteArrayElementAtIndex(i);
+                        serializedObject.ApplyModifiedProperties();
+                        UpdateFoldoutsArray();
+                        return; // Exit the loop to avoid issues with the modified array
+                    }
+
+                    EditorGUILayout.Space();
                 }
-
-                EditorGUILayout.PropertyField(tween.FindPropertyRelative("targetObject"));
-                EditorGUILayout.PropertyField(tween.FindPropertyRelative("duration"));
-                EditorGUILayout.PropertyField(tween.FindPropertyRelative("delay"));
-                EditorGUILayout.PropertyField(tween.FindPropertyRelative("easeType"));
-                EditorGUILayout.PropertyField(tween.FindPropertyRelative("loop"));
-                EditorGUILayout.PropertyField(tween.FindPropertyRelative("pingpong"));
-                EditorGUILayout.PropertyField(tween.FindPropertyRelative("loopCount"));
-                EditorGUILayout.PropertyField(tween.FindPropertyRelative("onComplete"));
-
-                EditorGUILayout.Space();
             }
         }
 
         if (GUILayout.Button("Add Tween Animation"))
         {
             tweener.tweenAnimations.Add(new Tweener.TweenSettings());
-            foldouts = new bool[tweenAnimations.arraySize];
+            serializedObject.ApplyModifiedProperties();
+            UpdateFoldoutsArray();
         }
 
         serializedObject.ApplyModifiedProperties();
--- Assets/_Game/Scripts/Utility/Tweener.cs
+++ Assets/_Game/Scripts/Utility/Tweener.cs
@@ -1,125 +1,65 @@
-using UnityEngine;
-using NaughtyAttributes;
 using DG.Tweening;
-using System.Collections;
+using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class Tweener : MonoBehaviour
 {
     [System.Serializable]
     public class TweenSettings
     {
-        public enum TweenType { Move, Scale, Rotate, Fade, Color }
+        public enum TweenType
+        {
+            Move,
+            Scale,
+            Rotate,
+            Fade,
+            Color
+        }
 
-        [BoxGroup("Type")]
-        [Tooltip("Jenis animasi yang akan dijalankan.")]
         public TweenType tweenType;
-
-        [BoxGroup("Target")]
-        [Tooltip("GameObject target animasi (default adalah GameObject ini).")]
-        public GameObject targetObject;
-
-        [BoxGroup("Target")]
-        [Tooltip("Apakah target adalah elemen UI.")]
-        public bool isUIElement = false;
-
-        [BoxGroup("Values")]
-        [ShowIf("")]
-        [Tooltip("Nilai target untuk Move, Scale, atau Rotate.")]
+        public bool isUIElement;
         public Vector3 targetValue;
-
-        [BoxGroup("Values")]
-        [Tooltip("Alpha target untuk animasi Fade (0 = transparan, 1 = sepenuhnya terlihat).")]
-        [Range(0f, 1f)] public float targetAlpha = 1f;
-
-        [BoxGroup("Values")]
-        [Tooltip("Warna target untuk animasi Color.")]
-        public Color targetColor = Color.white;
-
-        [BoxGroup("Timing")]
-        [Tooltip("Durasi animasi dalam detik.")]
-        [Min(0f)] public float duration = 1f;
-
-        [BoxGroup("Timing")]
-        [Tooltip("Delay sebelum animasi dimulai.")]
-        [Min(0f)] public float delay = 0f;
-
-        [BoxGroup("Easing")]
-        [Tooltip("Jenis easing yang akan digunakan.")]
-        public Ease easeType = Ease.Linear;
-
-        [BoxGroup("Looping")]
-        [Tooltip("Apakah animasi harus diulang?")]
-        public bool loop = false;
-
-        [BoxGroup("Looping")]
-        [Tooltip("Apakah animasi harus pingpong?")]
-        public bool pingpong = false;
-
-        [BoxGroup("Looping")]
-        [Tooltip("Jumlah loop animasi (0 untuk tidak terbatas).")]
-        public int loopCount = 0;
-
-        [BoxGroup("Events")]
-        [Tooltip("Callback saat animasi selesai.")]
-        public UnityEvent onComplete;
-
-        // Kondisi untuk NaughtyAttributes
-        private bool RequiresVectorValue => tweenType == TweenType.Move || tweenType == TweenType.Scale || tweenType == TweenType.Rotate;
-        private bool IsFadeType => tweenType == TweenType.Fade;
-        private bool IsColorType => tweenType == TweenType.Color;
+        public float duration;
+        public Ease easeType;
+        public float delay;
+        public bool loop;
+        public int loopCount;
+        public bool pingpong;
+        public float targetAlpha; // For Fade
+        public Color targetColor; // For Color
+        public bool startOnActive; // New property
+        public UnityEngine.Events.UnityEvent OnTweenComplete; // New property
+        public AnimationCurve customCurve; // New property
+        public bool useCustomCurve; // New property
     }
 
-    [BoxGroup("Global Settings")]
-    [Tooltip("Apakah mode debug aktif? Jika ya, log akan ditampilkan di console.")]
-    public bool debugMode = false;
-
-    [BoxGroup("Global Settings")]
-    [Tooltip("Apakah animasi dijalankan saat start.")]
-    public bool runOnStart = true;
-
-    [BoxGroup("Global Settings")]
-    [Tooltip("Apakah animasi menggunakan unscaled time.")]
     public bool useUnscaledTime = false;
-
-    [ReorderableList]
-    [BoxGroup("Tween Animations")]
-    [Tooltip("List animasi yang akan dijalankan secara berurutan.")]
     public List<TweenSettings> tweenAnimations = new List<TweenSettings>();
 
-    private void Start()
-    {
-        if (runOnStart)
-        {
-            StartCoroutine(ExecuteAnimations());
-        }
-    }
-
-    private IEnumerator ExecuteAnimations()
+    private void OnEnable()
     {
         foreach (var tween in tweenAnimations)
         {
-            RunTweenAnimation(tween);
-            yield return new WaitUntil(() => !DOTween.IsTweening(tween.targetObject));
-        }
-
-        if (debugMode)
-        {
-            Debug.Log("All animations completed.");
+            if (tween.startOnActive)
+            {
+                RunTween(tween, gameObject);
+            }
         }
     }
 
-    private void RunTweenAnimation(TweenSettings tween)
+    private void Start()
     {
-        GameObject target = tween.targetObject != null ? tween.targetObject : gameObject;
-
-        if (debugMode)
+        foreach (var tween in tweenAnimations)
         {
-            Debug.Log($"Starting {tween.tweenType} tween on {target.name}");
+            if (!tween.startOnActive)
+            {
+                RunTween(tween, gameObject);
+            }
         }
+    }
 
+    private void RunTween(TweenSettings tween, GameObject target)
+    {
         switch (tween.tweenType)
         {
             case TweenSettings.TweenType.Move:
@@ -131,124 +71,78 @@ public class Tweener : MonoBehaviour
             case TweenSettings.TweenType.Rotate:
                 RunRotateTween(tween, target);
                 break;
-            case TweenSettings.TweenType.Fade:
-                RunFadeTween(tween, target);
-                break;
-            case TweenSettings.TweenType.Color:
-                RunColorTween(tween, target);
-                break;
-            default:
-                if (debugMode)
-                {
-                    Debug.LogWarning($"Failed to execute {tween.tweenType} tween on {target.name}");
-                }
-                break;
+            // Add cases for Fade and Color if needed
         }
     }
 
     private void RunMoveTween(TweenSettings tween, GameObject target)
     {
-        var tweenAction = target.transform.DOMove(tween.targetValue, tween.duration)
-            .SetEase(tween.easeType)
-            .SetDelay(tween.delay)
-            .OnComplete(() => OnTweenComplete(tween))
-            .SetLoops(tween.loop ? -1 : tween.loopCount, tween.pingpong ? DG.Tweening.LoopType.Yoyo : DG.Tweening.LoopType.Restart);
-
-        if (useUnscaledTime)
+        Tween tweenAction;
+        if (tween.isUIElement && target.TryGetComponent(out RectTransform rectTransform))
         {
-            tweenAction.SetUpdate(true);
+            tweenAction = rectTransform.DOAnchorPos((Vector2)tween.targetValue, tween.duration);
         }
+        else
+        {
+            tweenAction = target.transform.DOMove(tween.targetValue, tween.duration);
+        }
+
+        ApplyTweenSettings(tween, tweenAction);
     }
 
     private void RunScaleTween(TweenSettings tween, GameObject target)
     {
-        var tweenAction = target.transform.DOScale(tween.targetValue, tween.duration)
-            .SetEase(tween.easeType)
-            .SetDelay(tween.delay)
-            .OnComplete(() => OnTweenComplete(tween))
-            .SetLoops(tween.loop ? -1 : tween.loopCount, tween.pingpong ? DG.Tweening.LoopType.Yoyo : DG.Tweening.LoopType.Restart);
-
-        if (useUnscaledTime)
+        Tween tweenAction;
+        if (tween.isUIElement && target.TryGetComponent(out RectTransform rectTransform))
         {
-            tweenAction.SetUpdate(true);
+            tweenAction = rectTransform.DOScale(tween.targetValue, tween.duration);
         }
+        else
+        {
+            tweenAction = target.transform.DOScale(tween.targetValue, tween.duration);
+        }
+
+        ApplyTweenSettings(tween, tweenAction);
     }
 
     private void RunRotateTween(TweenSettings tween, GameObject target)
     {
-        var tweenAction = target.transform.DORotate(tween.targetValue, tween.duration)
-            .SetEase(tween.easeType)
-            .SetDelay(tween.delay)
+        Tween tweenAction;
+        if (tween.isUIElement && target.TryGetComponent(out RectTransform rectTransform))
+        {
+            tweenAction = rectTransform.DORotate(tween.targetValue, tween.duration);
+        }
+        else
+        {
+            tweenAction = target.transform.DORotate(tween.targetValue, tween.duration);
+        }
+
+        ApplyTweenSettings(tween, tweenAction);
+    }
+
+    private void ApplyTweenSettings(TweenSettings tween, Tween tweenAction)
+    {
+        if (tween.useCustomCurve && tween.customCurve != null)
+        {
+            tweenAction.SetEase(tween.customCurve);
+        }
+        else
+        {
+            tweenAction.SetEase(tween.easeType);
+        }
+
+        tweenAction.SetDelay(tween.delay)
             .OnComplete(() => OnTweenComplete(tween))
             .SetLoops(tween.loop ? -1 : tween.loopCount, tween.pingpong ? DG.Tweening.LoopType.Yoyo : DG.Tweening.LoopType.Restart);
 
         if (useUnscaledTime)
         {
             tweenAction.SetUpdate(true);
-        }
-    }
-
-    private void RunFadeTween(TweenSettings tween, GameObject target)
-    {
-        if (target.TryGetComponent(out CanvasGroup canvasGroup))
-        {
-            var tweenAction = canvasGroup.DOFade(tween.targetAlpha, tween.duration)
-                .SetEase(tween.easeType)
-                .SetDelay(tween.delay)
-                .OnComplete(() => OnTweenComplete(tween))
-                .SetLoops(tween.loop ? -1 : tween.loopCount, tween.pingpong ? DG.Tweening.LoopType.Yoyo : DG.Tweening.LoopType.Restart);
-
-            if (useUnscaledTime)
-            {
-                tweenAction.SetUpdate(true);
-            }
-        }
-        else
-        {
-            Debug.LogWarning($"Failed to execute Fade tween on {target.name}");
-        }
-    }
-
-    private void RunColorTween(TweenSettings tween, GameObject target)
-    {
-        if (tween.isUIElement && target.TryGetComponent(out Graphic graphic))
-        {
-            var tweenAction = graphic.DOColor(tween.targetColor, tween.duration)
-                .SetEase(tween.easeType)
-                .SetDelay(tween.delay)
-                .OnComplete(() => OnTweenComplete(tween))
-                .SetLoops(tween.loop ? -1 : tween.loopCount, tween.pingpong ? DG.Tweening.LoopType.Yoyo : DG.Tweening.LoopType.Restart);
-
-            if (useUnscaledTime)
-            {
-                tweenAction.SetUpdate(true);
-            }
-        }
-        else if (target.TryGetComponent(out Renderer renderer))
-        {
-            var tweenAction = renderer.material.DOColor(tween.targetColor, tween.duration)
-                .SetEase(tween.easeType)
-                .SetDelay(tween.delay)
-                .OnComplete(() => OnTweenComplete(tween))
-                .SetLoops(tween.loop ? -1 : tween.loopCount, tween.pingpong ? DG.Tweening.LoopType.Yoyo : DG.Tweening.LoopType.Restart);
-
-            if (useUnscaledTime)
-            {
-                tweenAction.SetUpdate(true);
-            }
-        }
-        else
-        {
-            Debug.LogWarning($"Failed to execute Color tween on {target.name}");
         }
     }
 
     private void OnTweenComplete(TweenSettings tween)
     {
-        tween.onComplete?.Invoke();
-        if (debugMode)
-        {
-            Debug.Log($"Tween {tween.tweenType} completed for {tween.targetObject.name}");
-        }
+        tween.OnTweenComplete?.Invoke();
     }
 }
